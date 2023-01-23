6d36b38cc332cbc42885386d563f0ff6964d0833
Abdullah <88815725+Theonewhomadethings@users.noreply.github.com>
2023-01-23 02:54:07 +0000

    Add files via upload

---
+++ Assignment0/HelloWorld.java
@@ -0,0 +1,11 @@
+/* *****************************************************************************
+ *  Name:              Abdullah Saleem
+ *  Coursera User ID:  123456
+ *  Last modified:     January 21st 2023
+ **************************************************************************** */
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+    }
+}
